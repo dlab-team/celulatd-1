f9a865ae7b654db444210cf2e1a32be2519d4a2d
Israel Mege <70422956+ISTEMEFA@users.noreply.github.com>
2023-02-24 14:57:14 -0300

    proyecto react

---
+++ src/components/common/ContadorClisks.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import {sendCounterValue} from '../../services/CounterApi';
+
+
+function Counter({ icon, nombreContador ,link}) {
+  const [contador, setContador] = useState(0);
+
+  const handleClick = async () => {
+    setContador(contador + 1);
+    await sendCounterValue(nombreContador, contador + 1);
+    
+  };
+  return  (
+    <div onClick={() => window.location.href = link}>
+    {icon && (
+      <div onClick={handleClick}>
+        {React.createElement(icon)}
+       
+      </div>
+    )}
+  </div>
+  );
+}
+
+export default Counter;
