1dccd085e413b40dd8607d84fbc256c7fad76190
Israel Mege <70422956+ISTEMEFA@users.noreply.github.com>
2023-02-27 13:21:04 -0300

    correccion errores

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
-import Login from './components/user/Login';
-import Register from './components/user/Register';
-import Counter  from './components/common/ContadorClisks';
-import { FaFacebookF,FaTwitter,FaInstagram  } from 'react-icons/fa';
-import CarruselCalendario from './components/common/CarruselCalendario';
-import CarruselStories from './components/common/CarruselStories';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HistoriasUsuarios from './utils/HistoriasUsuarios';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Login/>
-       <Register/>
-       <Counter nombreContador = "FacebookF" icon ={FaFacebookF} link= 'https://www.facebook.com/'/>
-       <Counter nombreContador = "Twitter" icon ={FaTwitter} link= 'https://twitter.com/?lang=es' />
-       <Counter nombreContador = "Instagram" icon ={FaInstagram} link= 'https://www.instagram.com/' />
-       <CarruselCalendario/>
-       <CarruselStories/>
-      </header>
-      <div className="body">
-      
-    </div>
+    <h1>olaaaa</h1>
+   
     </div>
   );
 }
