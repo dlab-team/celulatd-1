c89961222c4ffe850b7276702c7b26b0ce093431
Carlos Leiva <122935392+CarlosRoLeiva@users.noreply.github.com>
2023-03-11 13:30:19 -0300

    actualizacion imagenes card

--- src/components/landing/body/SvgCard1.js
+++ src/components/landing/body/SvgCard1.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import '../../../scss/components/landing/body/_svgCard1.scss';
+import menorque from '../../../../src/assets/img/_Menor.svg'
 
 export default function SvgCard1() {
   return (
     <div>
         <div className='svg-card1'>
-          {'\<'}
+        <img src={menorque}/>
         </div>
     </div>
   )
