79b3c07601f711118ce774be5b55657369328e76
israel <israelmege@gmail.com>
2023-03-21 20:01:32 -0300

    feat(accesos): :art: Se agregan navbar con rutas
    Rutas para dashboard admin

--- src/components/dashboard/NavbarComponent.js
+++ src/components/dashboard/NavbarComponent.js
@@ -1,7 +1,5 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import { Link} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
@@ -14,26 +12,24 @@ function NavbarComponent() {
       {[false].map((expand) => (
         <Navbar key={expand} bg="light" expand={expand} className="mb-3">
           <Container fluid>
-          
-         
 					<Link to="/Inicio" className='Link'>
-						<Nav.Link>inicio</Nav.Link>
+          <Nav.Link href="/Inicio">inicio</Nav.Link>
 						</Link>
 						<Link to="/Ajustes" className='Link'>
-						<Nav.Link href="/Ajustes">ajustes</Nav.Link>
+            <Nav.Link href="/Ajustes">ajustes</Nav.Link>
 						</Link>
 						<Link to="/Perfil" className='Link'>
-						<Nav.Link href="/Perfil">perfil</Nav.Link>
+            <Nav.Link href="/Perfil">perfil</Nav.Link>
 						</Link>
             <Navbar.Brand href="#">Desafios Latam</Navbar.Brand>
 						<Link to="/SubirVideo" className='Link'>
-						<Nav.Link href="/SubirVideo">subir video</Nav.Link>
+					  <Nav.Link href="/SubirVideo">subir video</Nav.Link>
 						</Link>
-						<Link to="/ListarrVideo" className='Link'>
-						<Nav.Link href="/ListarrVideo">videos puplicados</Nav.Link>
+						<Link to="/ListarVideo" className='Link'>
+            <Nav.Link href="/ListarVideo">videos publicados</Nav.Link>
 						</Link>
 						<Link to="/Notification" className='Link'>
-						<Nav.Link href="/Notification">notificaciones</Nav.Link>
+            <Nav.Link href="/Notification">notificaciones</Nav.Link>
 						</Link>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
@@ -52,19 +48,11 @@ function NavbarComponent() {
             <Nav.Link href="/Ajustes">ajustes</Nav.Link>
             <Nav.Link href="/Perfil">perfil</Nav.Link>
             <Nav.Link href="/SubirVideo">subir video</Nav.Link>
-            <Nav.Link href="/ListarrVideo">videos puplicados</Nav.Link>
+            <Nav.Link href="/ListarVideo">videos publicados</Nav.Link>
             <Nav.Link href="/Notification">notificaciones</Nav.Link>
   
   </Nav>
-  <Form className="d-flex">
-    <Form.Control
-      type="search"
-      placeholder="Search"
-      className="me-2"
-      aria-label="Search"
-    />
-    <Button variant="outline-success">Search</Button>
-  </Form>
+  
 </Offcanvas.Body>
 </Navbar.Offcanvas>
 
