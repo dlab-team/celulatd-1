3943f29ac4f12cc54bdf3f573af9eeb96ca6bc68
Carlos Leiva <122935392+CarlosRoLeiva@users.noreply.github.com>
2023-04-17 12:09:56 -0400

    se integra la api de videos al site, y se le agregan los estilos correspondientes

--- src/components/dashboard/video/NavbarDocument.js
+++ src/components/dashboard/video/NavbarDocument.js
@@ -1,27 +1,25 @@
-import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-import NavbarComponent from '../NavbarComponent';
-import '../../../scssWeb/main.css';
-
-
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import "../../../scssWeb/main.css";
 
 function NavbarVideoRead() {
-
   return (
     <>
-
-      <div className='navbarvideo'>
-        <div className='container-video'>
+      <div className="navbarvideo">
+        <div className="container-video">
           <div>
-          <Link to="/ComponetVideo" className='Link'>
-            <Nav.Link href="/ComponetVideo"><h2>VIDEOS</h2></Nav.Link>
-          </Link>
+            <Link to="/ComponetVideo" className="Link">
+              <Nav.Link href="/ComponetVideo">
+                <h2>VIDEOS</h2>
+              </Nav.Link>
+            </Link>
           </div>
           <div>
-          <Link to="/Inicio" className='Link'>
-                      {/* redirigir a ver documentos */}
-            <Nav.Link href="/Inicio"><h2>DOCUMENTOS</h2></Nav.Link>
-          </Link>
+            <Link to="/Inicio" className="Link">
+              <Nav.Link href="/Inicio">
+                <h2>DOCUMENTOS</h2>
+              </Nav.Link>
+            </Link>
           </div>
         </div>
       </div>
@@ -30,4 +28,3 @@ function NavbarVideoRead() {
 }
 
 export default NavbarVideoRead;
-
