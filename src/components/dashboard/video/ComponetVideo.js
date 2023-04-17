0f2d641846951a137caac272a678fcc9a8aede70
israel <israelmege@gmail.com>
2023-04-17 12:17:36 -0400

    se cambia logica del componente de video

--- src/components/dashboard/video/ComponetVideo.js
+++ src/components/dashboard/video/ComponetVideo.js
@@ -12,12 +12,28 @@ const ComponetVideo = (props) => {
   const [name, setName] = useState(props.name);
   const [description, setDescription] = useState(props.description);
   const [uploadStatus, setUploadStatus] = useState("");
+  const [videos, setVideos] = useState([]);
+
 
   useEffect(() => {
-    setVideo(props.video);
-    setName(props.name);
-    setDescription(props.description);
-  }, [props.video, props.name, props.description]);
+    const API_KEY = "fpCsO7Yqx44FzOmNaNwHa64MqiGz6UCItjERAeDRLOfxH6nnt5OZja8K";
+    const PER_PAGE = 10;
+    const PAGE = 1;
+    const QUERY = "programming";
+
+
+    axios
+      .get(`https://api.pexels.com/videos/search?query=${QUERY}&per_page=${PER_PAGE}&page=${PAGE}`, {
+        headers: { Authorization: API_KEY }
+      })
+      .then(response => {
+        setVideos(response.data.videos);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
 
   const handleVideoChange = (event) => {
     setVideo(event.target.files[0]);
@@ -80,9 +96,9 @@ const ComponetVideo = (props) => {
       <div className="barra">
         <img className="svg-img-barra" src={RecursosSvg} />
         <h2>RECURSOS</h2>
-      </div>  
+      </div>
       <div className="container-body-all">
-      <NavbarVideoRead />
+        <NavbarVideoRead />
         <div className="container-componentvideo_flex">
           {isEditing ? (
             <div className="container-componentvideo_body">
@@ -105,20 +121,31 @@ const ComponetVideo = (props) => {
               </div>
             </div>
           ) : (
-            <div className="container-component-video-info">
-              <video src={video} controls />
-              <h2>{name}</h2>
-              <p>{description}</p>
-              <div className="body-button-edit">
-                <button onClick={handleEdit}>Editar</button>
-                <button onClick={handleDelete}>Eliminar</button>
+            <div className="container-componentvideo_flex">
+              {videos.map((video) => (
+                <div className="container-componentvideo_body" key={video.id}>
+                  <video src={video.video_files[0].link} controls />
+                  <h2>{video.user.name}</h2>
+                  <p>{video.url}</p>
+                  <div className="body-button-edit-all">
+                  <button onClick={() => handleDelete(video.id)}>Eliminar</button>
+                  <button onClick={() => console.log(`Editando video ${video.id}`)}>
+                    Editar
+                  </button>
+                  </div>
+                </div>
+              ))}
+              <div className="body-button-edit-all">
+                <button onClick={handleSave}>Guardar</button>
+                <button onClick={handleCancelEdit}>Cancelar</button>
               </div>
             </div>
+
           )}
           <p>{uploadStatus}</p>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
