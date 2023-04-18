ff18866cfc962682b43a5b0818b3c66e3ee90aef
israel <israelmege@gmail.com>
2023-04-17 22:23:02 -0400

    feat(Creacion de componente video): :art: se modifica compnente video d enuevo

--- src/components/dashboard/video/ComponetVideo.js
+++ src/components/dashboard/video/ComponetVideo.js
@@ -7,60 +7,31 @@ import Header from "../../../components/common/header.js";
 import "../../../scssWeb/main.css";
 
 const ComponetVideo = (props) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [video, setVideo] = useState(props.video);
-  const [name, setName] = useState(props.name);
-  const [description, setDescription] = useState(props.description);
-  const [uploadStatus, setUploadStatus] = useState("");
+
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    setVideo(props.video);
-    setName(props.name);
-    setDescription(props.description);
-  }, [props.video, props.name, props.description]);
+    const API_KEY = "fpCsO7Yqx44FzOmNaNwHa64MqiGz6UCItjERAeDRLOfxH6nnt5OZja8K";
+    const PER_PAGE = 10;
+    const PAGE = 1;
+    const QUERY = "programming";
 
-  const handleVideoChange = (event) => {
-    setVideo(event.target.files[0]);
-  };
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
-  };
-
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
-
-  const handleCancelEdit = () => {
-    setIsEditing(false);
-    setVideo(props.video);
-    setName(props.name);
-    setDescription(props.description);
-  };
-
-  const handleSave = () => {
-    const formData = new FormData();
-    formData.append("video", video);
-    formData.append("name", name);
-    formData.append("description", description);
     axios
-      .put(`/videos/${props.id}`, formData)
+      .get(
+        `https://api.pexels.com/videos/search?query=${QUERY}&per_page=${PER_PAGE}&page=${PAGE}`,
+        {
+          headers: { Authorization: API_KEY },
+        }
+      )
       .then((response) => {
-        setIsEditing(false);
-        setVideo(response.data.video);
-        setName(response.data.name);
-        setDescription(response.data.description);
-        setUploadStatus("Upload successful");
+        setVideos(response.data.videos);
       })
       .catch((error) => {
         console.log(error);
-        setUploadStatus("Upload failed");
       });
-  };
+  }, []);
+
+
 
   const handleDelete = () => {
     axios
@@ -77,45 +48,24 @@ const ComponetVideo = (props) => {
     <div className="container-responsive">
       <Header />
       <NavbarComp />
-      <div className="barra barra-navbar-video">
-        <img className="svg-img-barra" src={RecursosSvg} />
-        <NavbarVideo />
-      </div>
-      <div className="container-body-all">
-        <div className="container-componentvideo_flex">
-          {isEditing ? (
-            <div className="container-componentvideo_body">
-              <input type="file" name="video" onChange={handleVideoChange} />
-              <input
-                type="text"
-                name="name"
-                value={name}
-                onChange={handleNameChange}
-              />
-              <input
-                type="text"
-                name="description"
-                value={description}
-                onChange={handleDescriptionChange}
-              />
-              <div className="body-button-edit-all">
-                <button onClick={handleSave}>Guardar</button>
-                <button onClick={handleCancelEdit}>Cancelar</button>
-              </div>
+      <div className="container-componentvideo_flex">
+        {videos.map((video) => (
+          <div className="container-componentvideo_body" key={video.id}>
+            <video src={video.video_files[0].link} controls />
+            <h2>{video.user.name}</h2>
+            <p>{video.url}</p>
+            <div className="body-button-edit-all">
+              <button onClick={() => handleDelete(video.id)}>
+                Eliminar
+              </button>
+              <button
+                onClick={() => console.log(`Editando video ${video.id}`)}
+              >
+                Editar
+              </button>
             </div>
-          ) : (
-            <div className="container-component-video-info">
-              <video src={video} controls />
-              <h2>{name}</h2>
-              <p>{description}</p>
-              <div className="body-button-edit">
-                <button onClick={handleEdit}>Editar</button>
-                <button onClick={handleDelete}>Eliminar</button>
-              </div>
-            </div>
-          )}
-          <p>{uploadStatus}</p>
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
